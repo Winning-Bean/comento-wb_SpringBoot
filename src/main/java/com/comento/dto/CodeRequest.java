ac8dc51b48e35815375ebea1f0e911b557770213
Seungbeen Wee <wsb0722@naver.com>
2021-07-18 00:03:46 +0900

    상세코드 조건 조회 개발

--- src/main/java/com/comento/dto/CodeRequest.java
+++ src/main/java/com/comento/dto/CodeRequest.java
@@ -4,10 +4,10 @@ import lombok.Data;
 
 @Data
 public class CodeRequest {
-//    private String code;
+    private String commCdId;
 
-    @Data
-    public static class detail {
-        private String commCdId;
-    }
+//    @Data
+//    public static class detail {
+//        private String commCdId;
+//    }
 }
